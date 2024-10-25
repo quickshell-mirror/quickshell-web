c8cd2ec70e53e36a88989f0f848191615b81ae14
Xanazf <hotdamnsucka@gmail.com>
2024-10-25 04:47:13 +0300

    sitemap and robots.txt added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 import pagefind from "./pagefind";
 import { markdownConfig } from "./src/config/io/markdown.ts";
@@ -9,6 +10,7 @@ import { markdownConfig } from "./src/config/io/markdown.ts";
 // https://astro.build/config
 export default defineConfig({
   markdown: markdownConfig,
+  site: "https://quickshell.outfoxxed.me",
   integrations: [
     solidJs({
       devtools: true,
@@ -16,5 +18,6 @@ export default defineConfig({
     mdx(),
     pagefind(),
     icon(),
+    sitemap(),
   ],
 });
