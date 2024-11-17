69dbd9fa7bc2eed08f971cacc1f1af40c4066b4b
damii <deanam1507@gmail.com>
2024-11-17 22:15:46 +1100

    Fix rendering on webkit

--- shell.nix
+++ shell.nix
@@ -1,16 +1,20 @@
 {
   pkgs ? import <nixpkgs> {},
   quickshell-web ? pkgs.callPackage ./default.nix {},
-}: pkgs.mkShell {
-  inputsFrom = [ quickshell-web ];
+}:
+pkgs.mkShell {
+  inputsFrom = [quickshell-web];
 
-  nativeBuildInputs = [ (pkgs.writeShellScriptBin "yarn-install-hack" ''
-    set -x
-    yarn install || true
-    mkdir -p node_modules/node/bin
-    ln -sf ${pkgs.nodejs_22}/bin/node node_modules/node/bin/node
-    yarn install
-  '') ];
+  nativeBuildInputs = [
+    (pkgs.writeShellScriptBin "yarn-install-hack" ''
+      set -x
+      yarn install || true
+      mkdir -p node_modules/node/bin
+      ln -sf ${pkgs.nodejs_22}/bin/node node_modules/node/bin/node
+      yarn install
+    '')
+    pkgs.nodejs_22
+  ];
 
   YARN_NODE_LINKER = "node-modules";
 }
