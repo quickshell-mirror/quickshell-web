d1e1d3c529c99802953d32f2d6b4ef73c081dbca
Xanazf <hotdamnsucka@gmail.com>
2025-07-13 09:16:09 +0300

    added changelog and version dropdown

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,5 +8,12 @@ const guide = defineCollection({
     index: z.number(),
 	}),
 });
+const version = defineCollection({
+	loader: glob({ pattern: "**/*", base: "src/docs" }),
+	schema: z.object({
+		title: z.string(),
+    index: z.number(),
+	}),
+});
 
-export const collections = { guide };
+export const collections = { guide, version };
---
+++ src/pages/changelog.md
@@ -0,0 +1,8 @@
+---
+layout: "@layouts/GuideMdLayout.astro"
+title: Changelog
+---
+
+## v0.2.0
+
+## v0.1.0
