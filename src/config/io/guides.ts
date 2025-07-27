e7c807ac8595ba091d16328b9dfb956e5fe2c2ab
outfoxxed <outfoxxed@outfoxxed.me>
2025-07-26 19:39:33 -0700

    guide page versioning

---
+++ src/config/io/guides.ts
@@ -0,0 +1,35 @@
+import { type CollectionEntry, getCollection } from "astro:content";
+import { getVersionsData } from "@config/io/generateTypeData";
+
+// load latest version of each page for version
+async function buildGuideCollection(version: string): Promise<CollectionEntry<'guide'>[]> {
+  const { versions } = await getVersionsData();
+  const guidePages = await getCollection("guide");
+
+  const pages: { [key: string]: CollectionEntry<'guide'> } = {};
+
+  for (const currentVersion of versions.toReversed()) {
+    for (const page of guidePages) {
+      let [guideVersion, id] = page.id.split('/');
+      guideVersion = guideVersion.replaceAll('_', '.');
+      id = id ?? "index";
+      if (guideVersion !== currentVersion.name) continue;
+
+      pages[id] = { ...page, id };
+    }
+
+    if (currentVersion.name === version) break;
+  }
+
+  return Object.values(pages);
+}
+
+let guideCollections: { [key: string]: Promise<CollectionEntry<'guide'>[]> } = {};
+
+export async function getGuideCollection(version: string): Promise<CollectionEntry<'guide'>[]> {
+  if (!(version in guideCollections)) {
+    guideCollections[version] = buildGuideCollection(version);
+  }
+
+  return guideCollections[version];
+}
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const guide = defineCollection({
-	loader: glob({ pattern: "**/*", base: "src/guide" }),
+	loader: glob({ pattern: "**/*.md", base: "src/guide" }),
 	schema: z.object({
 		title: z.string(),
     index: z.number(),
