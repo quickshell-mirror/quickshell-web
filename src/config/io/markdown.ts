0a7fcbae5882647101e763cf65cda17972e472c4
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2026-04-11 15:56:18 -0600

    fix(a11y): fix aria-live typo on copy button

--- src/config/io/markdown.ts
+++ src/config/io/markdown.ts
@@ -165,7 +165,7 @@ const shikiCopyButton: ShikiTransformer = {
         class: "copy-button",
         role: "button",
         "aria-label": "Copy to clipboard",
-        "alia-live": "polite",
+        "aria-live": "polite",
         "data-code": this.source,
         onclick: `
                 navigator.clipboard.writeText(this.dataset.code);
